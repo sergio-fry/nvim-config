d6a0d4e74de9fd3bc78879d8756af55b4ea05ec3
Sergei O. Udalov <udalov.x@mail.ru>
2024-02-19 16:11:19 +0300

    lazy-vim russ keymaps

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.api.nvim_command("set spell spelllang=ru,en_us")
+vim.api.nvim_command("set keymap=russian-jcukenmac")
+
+-- english by default
+vim.api.nvim_command("set iminsert=0")
+vim.api.nvim_command("set imsearch=0")
